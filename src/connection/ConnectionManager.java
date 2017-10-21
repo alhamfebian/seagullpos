daa47ae5c23422b6f8a00ea45cb2e668d44d1de4
Dennis Kastoni <denniskastonii@gmail.com>
2017-10-22 01:56:24 +0700

    add openConnection and closeConnection method

--- src/connection/ConnectionManager.java
+++ src/connection/ConnectionManager.java
@@ -1,4 +1,50 @@
 package connection;
 
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+//using singleton design pattern
 public class ConnectionManager {
+
+    private static ConnectionManager connectionManager;
+    private static final String url = "jdbc:postgresql://localhost:5432/seagullpos";
+    private static Properties props;
+
+    private ConnectionManager(){
+        try{
+            Class.forName("org.postgresql.Driver");
+            props = new Properties();
+            props.setProperty("user", "postgres");
+            props.setProperty("password", "DENNIS12345");
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public static Connection createConnection() throws SQLException{
+        if(connectionManager == null){
+            connectionManager = new ConnectionManager();
+        }
+        try{
+            return DriverManager.getConnection(url, props);
+        }catch (SQLException e){
+            throw e;
+        }
+    }
+
+    public static void closeConnection(Connection connection){
+        try{
+            if(connection != null){
+                connection.close();
+            }
+        }catch (SQLException sq){
+            sq.printStackTrace();
+        }
+    }
 }
